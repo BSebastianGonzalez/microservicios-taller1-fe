fb89f7494681d5677ffcb8e2b82d3fceab614d4a
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-10-01 07:18:16 -0500

    hotfix: fixing file uploading feature

--- pdau/src/api/axios.js
+++ pdau/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://microservicios-taller1-be-production.up.railway.app',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
